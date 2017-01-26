dcbc14be873019019eb00671dfb3851a6ca9bed2
quicotte-formation <tom@10.100.198.209>
2017-01-26 17:01:50 +0100

    V3

---
+++ req.sql
@@ -0,0 +1,26 @@
+1. Ajouter une action ds TestController qui liste
+tous produits dont le prix est compris entre 100 et 5000 euros.
+( BETWEEN )
+
+2. Tous les produits dont l'id est 1, 2, ou 3 ( IN )
+
+3. Les commandes passées par Antoine
+
+4. Commandes passées par Wars, dans l'ordre chronologique
+
+5. Les produits commandés par par Florence, dans l'ordre alpha
+
+6. Les commandes comprenant un pelle
+
+7. Les produits ayant fait l'objet d'une commande
+
+8. Les produits n'ayant pas fait l'objet d'une commande
+
+9. Les produits ayant fait l'objet d'une commande de la part de Florence,
+qui n'ont pas fait l'objet d'une commande de Trump
+
+10. Le nom de chaque produit, et le nombre de fois qu'ils ont été commandés
+(GROUP BY), classés par ordre alpha
+
+11. Idem que 10, uniquement si ils ont été commandés au moins 2 x
+
--- src/AppBundle/Controller/TestController.php
+++ src/AppBundle/Controller/TestController.php
@@ -8,7 +8,28 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class TestController extends Controller
 {
     /**
-     * @Route("/lister_produits")
+     * @Route("/com_cli_1_prod_1", name="com_cli_1_prod_1")
+     */
+    public function listerCommandesClient1Prod1Action(){
+        
+        // Requete DB
+        $dlq = ""
+                . "SELECT   cmd "
+                . "FROM     AppBundle:Commande cmd "
+                . "         JOIN cmd.client cli "
+                . "         JOIN cmd.produits prod "
+                . "WHERE    cli.id=1 "
+                . "         AND prod.id=1";
+        $commandes = $this->getDoctrine()->getManager()->createQuery( $dlq )->getResult();
+        
+        // Renvoie vers vue twig
+        return $this->render("AppBundle:Test:com_cli_1_prod_1.html.twig", 
+                array( "commandes"=>$commandes ));
+    }
+
+
+    /**
+     * @Route("/lister_produits", name="lister_produits")
      */
     public function listerProduitsAction(){
         
@@ -18,14 +39,23 @@ class TestController extends Controller
         
         // Envoie la var 'mesProduits' 
         return $this->render("AppBundle:Test:lister_produits.html.twig",
-                array('mesProduits'=>$produits));
+                array('mesProduits'=>$produits,
+                      'titre'=>"Liste de ts les produits"));
     }
     
     /**
-     * @Route("")
+     * @Route("/lister_prod_par_cat_id", name="lister_prod_par_cat_id")
      */
     public function prodParCatAction(){
         
+        // Recup produits par catid en BD
+        $em = $this->getDoctrine()->getManager();
+        $produits = $em->createQuery("SELECT p FROM AppBundle:Produit p JOIN p.categories c WHERE c.id=2")->getResult();
+        
+        // Envoie la var 'mesProduits' 
+        return $this->render("AppBundle:Test:lister_produits.html.twig",
+                array('mesProduits'=>$produits,
+                      'titre'=>"Liste des produits d'id 2"));
     }
     
     /**
