e337429f1514984e0f18aaba1b1b1d5bb2c23b6d
quicotte-formation <tom@10.100.198.209>
2017-02-01 16:25:36 +0100

    Solutions sécurité v2.

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -4,9 +4,34 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
 class SecurityController extends Controller
 {
+    /**
+     * @Route("secuByController")
+     */
+    public function secuByControllerAction(){
+        
+        $this->denyAccessUnlessGranted("ROLE_ADMIN");
+        
+        return $this->render('AppBundle:Security:firewall.html.twig', array(
+            // ...
+        ));
+    }
+
+
+    /**
+     * @Route("/secuByAnnotation")
+     * @Security("has_role('ROLE_USER')")
+     */
+    public function secuByAnnotationAction(){
+        
+        return $this->render('AppBundle:Security:firewall.html.twig', array(
+            // ...
+        ));
+    }
+    
     /**
      * @Route("/firewall")
      */
