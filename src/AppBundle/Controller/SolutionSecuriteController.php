e741871cbbd53eff485b0f2563a11c60f7f6f29f
quicotte-formation <tom@10.100.198.209>
2017-02-05 16:12:32 +0100

    v2

--- src/AppBundle/Controller/SolutionSecuriteController.php
+++ src/AppBundle/Controller/SolutionSecuriteController.php
@@ -5,46 +5,74 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
-class SolutionSecuriteController extends Controller
-{
+class SolutionSecuriteController extends Controller {
+
+    /**
+     * @Route("/programmaticSecu", name="programmaticSecu")
+     */
+    public function programmaticSecuAction(){
+        $this->denyAccessUnlessGranted( 'ROLE_ADMIN' );
+        
+        return $this->render('AppBundle:SolutionSecurite:secu1.html.twig', array(
+                        // ...
+        ));
+    }
+    
+    /**
+     * @Route("login", name="login")
+     */
+    public function loginAction() {
+        
+        // Récupère service d'authentification
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        // Récupère dernière err de login si existe
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // Dernier nom d'util entré par l'utilisateur
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        // Renvoie vers le formulaire de login
+        return $this->render('AppBundle:SolutionSecurite:login.html.twig', array(
+                    'last_username' => $lastUsername,
+                    'error' => $error,
+        ));
+    }
+
     /**
      * @Route("/anonSecu1")
      */
-    public function anonSecu1Action()
-    {
+    public function anonSecu1Action() {
         return $this->render('AppBundle:SolutionSecurite:secu1.html.twig', array(
-            // ...
+                        // ...
         ));
     }
 
     /**
      * @Route("/userSecu1")
      */
-    public function userSecu1Action()
-    {
+    public function userSecu1Action() {
         return $this->render('AppBundle:SolutionSecurite:secu2.html.twig', array(
-            // ...
+                        // ...
         ));
     }
 
     /**
      * @Route("/adminSecu3")
      */
-    public function adminSecu3Action()
-    {
-        
+    public function adminSecu3Action() {
+
         return $this->render('AppBundle:SolutionSecurite:admin_secu3.html.twig', array(
-            // ...
+                        // ...
         ));
     }
 
     /**
      * @Route("/adminSecu4")
      */
-    public function adminSecu4Action()
-    {
+    public function adminSecu4Action() {
         return $this->render('AppBundle:SolutionSecurite:admin_secu4.html.twig', array(
-            // ...
+                        // ...
         ));
     }
 
